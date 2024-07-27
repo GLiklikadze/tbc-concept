a07d0485d3e9543780b1520978a2842cb8e3beda
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2024-07-27 22:55:08 +0400

    add sections: numbers, offers, digital_bank, offers_products, banner, awards

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+console.log("hello world");
+document.addEventListener("DOMContentLoaded", function () {});
+let swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  direction: "horizontal",
+  speed: 300,
+  spaceBetween: 20,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".main-slider-next",
+    prevEl: ".main-slider-prev",
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggdraggable: true,
+    dragSize: 300,
+    hide: false,
+  },
+});
+let updateScrollWidth = () => {
+  let allSlides = swiper.slides.length;
+  let visibleSlides = swiper.params.slidesPerView;
+  let scrollDrag = document.querySelector(".swiper-scrollbar-drag");
+  if (scrollDrag) {
+    scrollDrag.style.width = `${(visibleSlides / allSlides) * 100}%`;
+  }
+};
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let swiper = new Swiper(".swiper1", {
+//     // Optional parameters
+//     direction: "horizontal",
+//     loop: false,
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".main-slider-next",
+//       prevEl: ".main-slider-prev",
+//     },
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: ".swiper1-scrollbar",
+//       hide: false,
+//       draggdraggable: true,
+//       dragSize: 300,
+//     },
+//   });
+// });
